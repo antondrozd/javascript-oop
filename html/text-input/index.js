350658fef679dfbd0d69e48810f673fb2783e12d
Anton Drozd <anton.drozd.95@gmail.com>
2018-04-27 03:51:07 +0300

    TextInput

--- html/text-input/index.js
+++ html/text-input/index.js
@@ -2,21 +2,27 @@ const Input = require('../input');
 
 class TextInput extends Input {
     /**
-     * @param {{ tag: string, id: string, className: string, type: string, name: string, disabled: boolean, required: boolean, value: string, minlength: string, maxlength: string }} args 
+     * @param {{ tag: string, id: string, className: string, type: string, name: string, disabled: boolean, required: boolean, value: string, minlength: string, maxlength: string }} args
      */
     constructor({ value = '', minlength, maxlength, ...args }) {
-        
+        super({ ...args, value });
+
+        this.minlength = minlength;
+        this.maxlength = maxlength;
     }
 
     /**
      * Проверяет валидность значения.
-     * 
+     *
      * Значение должно быть больше или равно `minlength` и меньше или равно `maxlength`.
-     * 
+     *
      * @returns {boolean}
      */
     get isValid() {
-        
+        return (
+            this.value.length >= this.minlength &&
+            this.value.length <= this.maxlength
+        );
     }
 }
 
