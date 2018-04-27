13874e8521f6789b4d44e8bd3760505015d59092
Anton Drozd <anton.drozd.95@gmail.com>
2018-04-27 03:56:55 +0300

    EmailInput

--- html/email-input/index.js
+++ html/email-input/index.js
@@ -2,22 +2,24 @@ const TextInput = require('../text-input');
 
 class EmailInput extends TextInput {
     /**
-     * @param {{ tag: string, id: string, className: string, type: string, name: string, disabled: boolean, required: boolean, value: string, minlength: string, maxlength: string }} args 
+     * @param {{ tag: string, id: string, className: string, type: string, name: string, disabled: boolean, required: boolean, value: string, minlength: string, maxlength: string }} args
      */
     constructor(args) {
-        
+        super(args);
     }
-    
+
     /**
      * Проверяет валидность значения
-     * 
+     *
      * Email RegEx: `/^[a-zA-Z0-9'._%+-]+@[a-zA-Z0-9-][a-zA-Z0-9.-]*\.[a-zA-Z]{2,63}$/`
-     * 
+     *
      * Для проверки можно использовать метод `regex.test(value)`
      * @returns {boolean}
      */
     get isValid() {
+        const emailRegEx = /^[a-zA-Z0-9'._%+-]+@[a-zA-Z0-9-][a-zA-Z0-9.-]*\.[a-zA-Z]{2,63}$/;
         
+        return emailRegEx.test(this.value);
     }
 }
 
