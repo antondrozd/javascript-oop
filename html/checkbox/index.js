adac9c7016e364922e6c380744712666990b4881
Anton Drozd <anton.drozd.95@gmail.com>
2018-04-27 03:44:02 +0300

    Checkbox

--- html/checkbox/index.js
+++ html/checkbox/index.js
@@ -2,28 +2,36 @@ const Input = require('../input/');
 
 class Checkbox extends Input {
     /**
-     * @param {{ tag: string, id: string, className: string, type: string, name: string, disabled: boolean, required: boolean, checked: boolean }} args 
+     * @param {{ tag: string, id: string, className: string, type: string, name: string, disabled: boolean, required: boolean, checked: boolean }} args
      */
     constructor({ checked = false, ...args }) {
-        
+        super({ ...args, type: 'checkbox', value: false });
+
+        this._checked = checked;
     }
 
     /**
      * `get` - Возвращает значение элемента.
-     * 
+     *
      * `set` - Устанавливает значение элемента и сообщает слушателям о событии `change`.
-     * 
+     *
      * @returns {boolean}
      */
-    get checked() {  }
-    set checked(value) {  }
+    get checked() {
+        return this._checked;
+    }
+
+    set checked(value) {
+        this._checked = !this._checked;
+        this.emit('change', value);
+    }
 
     /**
      * Проверяет валидность значения
      * @returns {boolean}
      */
     get isValid() {
-        
+        return typeof this._checked === 'boolean';
     }
 }
 
