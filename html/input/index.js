006692bb091f5a32e5401e6c2fe8a887f7deb34a
Anton Drozd <anton.drozd.95@gmail.com>
2018-04-27 03:28:25 +0300

    Update Input

--- html/input/index.js
+++ html/input/index.js
@@ -45,7 +45,7 @@ class Input extends Element {
      * @returns {boolean}
      */
     get isValid() {
-        this.required && this.value == undefined ? false : true;
+        return this.required && this.value == undefined ? false : true;
     }
 }
 
